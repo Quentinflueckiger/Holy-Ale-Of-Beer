4f3ad702471a0f3c510dc041031d7be5aeb2951b
Adrian Aulbach <adrian@aulbach.ch>
2014-09-24 13:21:11 +0200

    generate SQL

--- php/byRegion.php
+++ php/byRegion.php
@@ -23,9 +23,33 @@ if(isset($_GET['region'])) {
 		}
 			
 		if(!is_numeric($_GET['region'])) {
+			//search regions by name
 			$query = 'SELECT `ID` FROM `region` WHERE `name` LIKE "%'.$_GET['region'].'%"';
-			
+			$res = $db->query($query);
+			while($obj = $res->fetchObject()) {
+				$IDs[] = $obj->ID;
+			}
+		} else {
+			//region is given by ID
+			$IDs[] = $_GET['region'];
 		}
+		
+		//Get sub-regions (child elements)
+		$IDsTmp = $IDs;
+		foreach($IDsTmp as $ID) {
+			$IDs = array_merge($IDs, children($ID));
+		}
+		
+		//generate SQL-query
+		$query = "SELECT 
+`brand`.`ID`,`brand`.`name`, `beer`.`ID`, `beer`.`name`, `beer`.`url`, `region`.`ID`, `region`.`name`
+FROM `beer`
+JOIN `brand` ON `beer`.`brand` = `brand`.`ID`
+JOIN `region` ON `brand`.`region` = `region`.`ID`";
+		//add where statement
+		$query = $query."\n WHERE `brand`.`region` = ".implode($IDs, " OR ");
+		
+		echo "\n".$query."\n";
 		
 	} else {
 		//ToDo: Datenbankverbindung gescheitert
