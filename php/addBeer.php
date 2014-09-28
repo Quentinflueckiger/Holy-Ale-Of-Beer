13966c73fb7ed2244cc97e9500b475bb53e5964c
Adrian Aulbach <adrian@aulbach.ch>
2014-09-28 03:01:13 +0200

    minor changes

--- php/addBeer.php
+++ php/addBeer.php
@@ -1,27 +1,35 @@
 <?php
-echo var_dump($_POST);
 include_once("config.php");
 $db = new mysqli(DB_HOST, DB_USER, DB_PW, DB_NAME);
-
+session_start();
 
 if(isset($_POST['name'])) {
-		
-	$query = "INSERT INTO `beer` ".
-		"`name` = ".$db->escape_string($_POST['name']).",".
-		"`url` = ".$db->escape_string($_POST['url']);
-	
-	echo $query;	
-	
-	if(is_numeric($_POST['brand'])) {
-		$query = $query.",`brand` = ".$_POST['brand'];
+	if(!is_numeric($_POST['brand'])) {
+		$_POST['brand'] = 0;
 	}
-	if(is_numeric($_POST['region'])) {
-		$query = $query.",`region` = ".$_POST['region'];
-	}
-	echo $query;
+	if(!is_numeric($_POST['region'])) {
+		$_POST['region'] = 0;
+	}	
+	
+	$query = "INSERT INTO `beer` (`name`, `url`, `brand`, `region`)".
+		'VALUES ("'.$_POST['name'].'", "'.$_POST['url'].'", "'.$_POST['brand'].'", "'.$_POST['region'].'")';
+
 	$res = $db->query($query);
+	$ID = $db->insert_id;
+
+	if($_POST['region'] == 0) {
+		$_SESSION['regionRequired']['beer'] = $ID;
+	}
+	if($_POST['brand'] == 0) {
+		$_SESSION['brandRequired']['beer'] = $ID;
+	}
 	
-//	die;
+	if(isset($_SESSION['regionRequired']['beer'])) {
+		header("Location: addRegion.php");
+	} elseif(isset($_SESSION['brandRequired']['beer'])) {
+		header("Location: addBrand.php");
+	}
+	
 } else {
 ?>	
 <html>
