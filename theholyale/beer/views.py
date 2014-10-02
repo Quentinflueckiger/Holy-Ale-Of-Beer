b1929abec586bfb6d1b02cf37bf4e7dab74ac39f
Adrian Aulbach <adrian@aulbach.ch>
2014-10-02 07:13:16 +0200

    prototype of beer listing

--- theholyale/beer/views.py
+++ theholyale/beer/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from django.template import RequestContext, loader
+from beer.models import Beer
 from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-	return HttpResponse("Index of Beer")
+	beer_list = Beer.objects.order_by('name')
+	template = loader.get_template('beer/index.html')
+	context = RequestContext(request, {'beer_list' : beer_list})
+	return HttpResponse(template.render(context))
+
+
 def detail(request, beer_id):
 	return HttpResponse("You're looking at beer #" + beer_id)
