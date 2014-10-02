f6d59318ca7dd4a0bb28f941fbacdb319f6d11f5
Adrian Aulbach <github@aulbach.ch>
2014-10-02 16:17:35 +0200

    routing for 'rate'

--- theholyale/beer/urls.py
+++ theholyale/beer/urls.py
@@ -4,4 +4,5 @@ from beer import views
 urlpatterns = patterns('',
 	url(r'^$', views.index, name='index'),
 	url(r'^(?P<beer_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<beer_id>[0-9]+)/rate$', views.rate, name='rate'),
 )
