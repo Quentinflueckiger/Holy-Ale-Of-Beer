105cc7acf4f0c3de4a7c894b32455e7c0ce16278
Adrian Aulbach <adrian@aulbach.ch>
2014-10-06 14:05:43 +0200

    Beers can now be rated

--- theholyale/beer/views.py
+++ theholyale/beer/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.template import RequestContext, loader
-from beer.models import Beer
-from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from beer.models import Beer, Rating
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.core.urlresolvers import reverse
 
 # Create your views here.
 def index(request):
@@ -20,4 +21,12 @@ def detail(request, beer_id):
 	return render(request, 'beer/detail.html', {'beer':beer})
 
 def rate(request, beer_id):
-	return HttpResponse("You're rating beer %s." % beer_id)
+	beer = get_object_or_404(Beer, pk=beer_id)
+	try:
+		r = request.POST['rating']
+		c = request.POST['comment']
+	except(KeyError):
+		return render(request, 'beer/detail', {'beer':beer})
+	else:
+		Rating.objects.create(beer=Beer.objects.get(pk=beer_id), rating=int(r), comment=c)
+		return HttpResponse("You're rating the beer with  %s stars" %  r)
