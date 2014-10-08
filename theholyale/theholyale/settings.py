952f3da02e667dc5f0a9c91a86c9413c5bbe4eef
Adrian Aulbach <adrian@aulbach.ch>
2014-10-08 15:52:01 +0200

    setting template dir to for test '/storage/sites/theholyale/theholyale/theholyale/templates/theholyale'

--- theholyale/theholyale/settings.py
+++ theholyale/theholyale/settings.py
@@ -24,7 +24,7 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
-TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'theholyale/templates/theholyale'))#'/theholyale/templates/theholyale')
+TEMPLATE_DIRS = (os.path.join(BASE_DIR, '/storage/sites/theholyale/theholyale/theholyale/templates/theholyale'),)#'/theholyale/templates/theholyale')
 
 
 ALLOWED_HOSTS = []
