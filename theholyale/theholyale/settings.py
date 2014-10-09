2eae289d81c87b7cfd5ac2b261bb1eec4c84c958
Adrian Aulbach <adrian@aulbach.ch>
2014-10-09 10:13:24 +0200

    adding ',' at the end of STATICFILES_DIRS

--- theholyale/theholyale/settings.py
+++ theholyale/theholyale/settings.py
@@ -89,5 +89,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 	os.path.join(BASE_DIR, "static/css"),
-	os.path.join(BASE_DIR, "static/img")
+	os.path.join(BASE_DIR, "static/img"),
 )
