df9b3016897f1c8788b96de0791c8f31a0eaa8ab
Adrian Aulbach <adrian@aulbach.ch>
2014-10-13 23:30:08 +0200

    disabled images in models

--- theholyale/beer/models.py
+++ theholyale/beer/models.py
@@ -5,7 +5,7 @@ class Brand(models.Model):
 	name = models.CharField(max_length=100)
 	description = models.CharField(max_length=200, blank=True)
 	url = models.URLField(max_length=200, blank=True)
-	logo = models.ImageField(upload_to="brand", blank=True)
+#	logo = models.ImageField(upload_to="brand", blank=True)
 	def __unicode__(self):
 		return self.name
 
@@ -14,7 +14,7 @@ class Beer(models.Model):
 	description = models.CharField(max_length=200, blank=True)
 	url = models.URLField(max_length=200, blank=True)
 	brand = models.ForeignKey(Brand)
-	picture =  models.ImageField(upload_to="beer", blank=True)
+#	picture =  models.ImageField(upload_to="beer", blank=True)
 	def __unicode__(self):
 		return self.brand.name  +" "+  self.name
 	
